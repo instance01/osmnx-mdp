2913945d52049a0b8f0f59b7cdd7a43cc9b0cb60
Instance <d.ratke@campus.lmu.de>
2019-08-11 09:04:45 +0200

    Add cpp_queue_util.hpp

---
+++ osmnx_mdp/cpp_queue_util.hpp
@@ -0,0 +1,32 @@
+#ifndef QUEUE_UTIL_HEADER
+#define QUEUE_UTIL_HEADER
+#include <vector>
+#include <algorithm>
+
+template<typename I, typename P>
+I queue_pop(std::vector<std::pair<I, P>> &queue) {
+    std::pop_heap(
+            queue.begin(),
+            queue.end(),
+            [](auto &item1, auto &item2) { return item1.second > item2.second; });
+    I node = queue.back().first;
+    queue.pop_back();
+    return node;
+}
+
+template<typename I, typename P>
+void queue_decrease_priority(std::vector<std::pair<I, P>> &queue, I y, P priority) {
+    auto it = std::find_if (
+            queue.begin(),
+            queue.end(),
+            [y](auto &item) { return item.first == y; });
+
+    if (it == queue.end()) {
+        queue.push_back({y, priority});
+    } else {
+        queue[std::distance(queue.begin(), it)] = {it->first, priority};
+    }
+
+    std::make_heap(queue.begin(), queue.end());
+}
+#endif
